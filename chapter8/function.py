794addde65b1cab615fbf7668b78e46fad47bdf4
liza kapopara <lizakapopara8@gmail.com>
2024-12-26 12:19:25 +0530

    Add files via upload

---
+++ chapter8/function.py
@@ -0,0 +1,8 @@
+
+# function definition
+def func1():
+    print("liza")
+# function call
+func1()
+
+# jyare pan function call karse ne tyare je pan function ma hase ne ae code execute thase
---
+++ chapter8/function_with_arguments.py
@@ -0,0 +1,9 @@
+def goodday(name, ending):
+    print("Good Day,"+name)
+    print(ending)
+    return "ok"
+
+goodday("liza", "Thank you")
+goodday("krish", "Thank you")
+a = goodday("vrund", "Thank you")
+print(a)
